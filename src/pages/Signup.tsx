c2443c0fdb73798804c3646ac361a39f0947d43a
abhu-u <itsudit.py@gmail.com>
2025-10-14 18:10:38 +0530

    Signup.tsx

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -47,7 +47,7 @@ const Signup = () => {
 
     try {
       setLoading(true);
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("http://3.138.37.238:5000/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
