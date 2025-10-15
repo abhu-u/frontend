eaa54c495092d120d1ca9b871bcb0bbe82b3a5b7
abhu-u <itsudit.py@gmail.com>
2025-10-15 15:18:35 +0530

    Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -32,7 +32,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("https://thin-desktop-having-mission.trycloudflare.com", {
+      const response = await fetch("https://testament-societies-plus-trader.trycloudflare.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
